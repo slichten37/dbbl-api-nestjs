69ca8659b3f3cec5867905a9d304968bbf27b88c
Spencer Lichtenberg <spencer.lichtenberg@gmail.com>
2026-02-16 21:54:14 -0500

    scaffold

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import {
+  INestApplication,
+  Injectable,
+  Logger,
+  OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  private readonly logger = new Logger(PrismaService.name);
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    await app.close();
+  }
+}
