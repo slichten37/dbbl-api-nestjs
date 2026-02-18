45a7863fa2f891527a886d771997586f3c109a51
Spencer Lichtenberg <spencer.lichtenberg@gmail.com>
2026-02-17 23:21:46 -0500

    enable cors blocking

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3000);
